3842dd8da4a1e556ded04984510a63fa4c0aedb8
sara charafi <elkbircharafi8@gmail.com>
2026-03-06 11:20:04 +0100

    create StatusBadge component

--- src/components/StatusBadge.jsx
+++ src/components/StatusBadge.jsx
@@ -3,6 +3,12 @@ export function StatusBadge(status){
   if(status === "Upcoming") return `<span class="badge rounded-pill bg-primary ms-2">${status}</span>`;
   if(status === "Pending") return `<span class="badge rounded-pill bg-warning text-dark ms-2">${status}</span>`;
   return `<span class="badge rounded-pill bg-secondary ms-2">${status}</span>`;
+
+
+
+
+
+
   
 }
 
